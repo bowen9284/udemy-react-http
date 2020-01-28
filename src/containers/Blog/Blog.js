87a3fb48c39236fe04b23320d0dd88e504870412
Matt Bowen <bowen9284@gmail.com>
2020-01-27 21:47:13 -0500

    intial commit and using axios with dummy api

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: 'Max'
+        };
+      });
+      this.setState({ posts: updatedPosts });
+    });
+  }
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title} author={post.author} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
