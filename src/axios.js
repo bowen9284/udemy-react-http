e1e485c7f092b551cdbacd82b80857b321907554
Matt Bowen <bowen9284@gmail.com>
2020-01-30 21:51:36 -0500

    axios instances

---
+++ src/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+
+// interceptors can be used in this way too
+// instance.interceptors.request()
+
+export default instance;
